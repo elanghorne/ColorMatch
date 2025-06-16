b815a0b43e71de68a3957729b64539c9ed2fdc1c
elanghorne <ericlanghorne7@gmail.com>
2025-06-16 15:26:29 -0400

    Initial Commit

---
+++ ColorMatch/ColorMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorMatchApp.swift
+//  ColorMatch
+//
+//  Created by Eric Langhorne on 6/16/25.
+//
+
+import SwiftUI
+
+@main
+struct ColorMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
